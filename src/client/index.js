f9b7511aabcf3979f1d5d6e1b9516827a009a288
ahmed <ahmedibrahimkenawi@gmail.com>
2020-06-05 06:21:21 +0200

    import rectangle.png in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,9 @@
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
 
+import img from './images/rectangle.png';
+
+
 console.log(checkForName);
 
 alert("I EXIST");
